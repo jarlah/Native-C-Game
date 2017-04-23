d47f11a84c822a46d51a1b225cb27fa9cb8d932c
Jarl André Hübenthal <jarl.andre@gmail.com>
2017-04-23 21:35:57 +0200

    Fixing close button

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+const int FRAME_RATE = 60;
 
 typedef struct {
     SDL_Window* window;
@@ -63,15 +64,33 @@ int main(int argc, char *args[]) {
         printf( "Unable to initialize game %s", game.error);
         return 1;
     }
+
     SDL_Surface* helloWorld = SDL_LoadBMP( "assets/hello_world.bmp" );
-    if (helloWorld != NULL) {
-        SDL_BlitSurface(helloWorld, NULL, game.surface, NULL );
-    } else {
-        printf( "Unable to load image %s! SDL Error: %s\n", "assets/hello_world.bmp", SDL_GetError() );
-        return 2;
+
+    // set to 1 when window close button is pressed
+    int close_requested = 0;
+
+    // animation loop
+    while (!close_requested) {
+        // process events
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                close_requested = 1;
+            }
+        }
+        if (helloWorld != NULL) {
+            SDL_BlitSurface(helloWorld, NULL, game.surface, NULL );
+        } else {
+            printf( "Unable to load image %s! SDL Error: %s\n", "assets/hello_world.bmp", SDL_GetError() );
+            return 2;
+        }
+        SDL_UpdateWindowSurface(game.window);
+        // wait 1/60th of a second
+        SDL_Delay(1000/60);
     }
-    SDL_UpdateWindowSurface(game.window);
-    SDL_Delay(2000);
+
     closeGame(game, 1, helloWorld);
+
     return 0;
 }
